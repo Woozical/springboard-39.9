87bad117406b2321fb8203efc362b643e53e248a
Mark <mark@example.dev>
2021-11-16 03:48:17 -0800

    card component

--- src/Card.js
+++ src/Card.js
@@ -1,5 +1,18 @@
+// returns an image
+// props: src (image link), alt text, zIndex, rotation
 const Card = (props) => {
-  return <div></div>
-}
+  const transforms = {
+    zIndex: props.index,
+    transform: `rotate(${props.rotation}turn)`
+  };
+  return <img src={props.src} alt={props.alt} style={transforms}></img>
+};
+
+Card.defaultProps = {
+  rotation: Math.random(),
+  alt: 'card',
+  src: '',
+  index: 0
+};
 
 export default Card;
